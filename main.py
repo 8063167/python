2913c0a97fcec4eff9ec1cfbdfeef6d055ff04e2
Alejandro Gutierrez <jorgele84@hotmail.com>
2019-04-04 20:34:09 -0500

    cambio uno

---
+++ main.py
@@ -0,0 +1,29 @@
+""" x =int (input("ingrese dato"))
+#print(x)
+y = input("ingrese dato")
+print(y + str(x))
+a = True
+b = False
+
+print(a) """
+
+# a = 5
+# if a < 4:
+#     print("hola")
+#     print("mundo")
+# elif a < 5
+# print("dgdfg")
+# else:
+#     print("no es menor")
+
+# a = 7
+# while a ==7:
+#     print("estoy en el while")
+#     a = int(input("ingrese el nuevo valor de a"))
+#     if a != 7:
+#         break
+
+a = 3
+b = 4
+if a == 3 and b == 4:
+    print("es correcto")
